5a2a12da0485623d1c4f36c0c1625e1ef9ad1dce
Miguel-M-Oliveira <miguel.oliveira2000@hotmail.com>
2021-03-11 06:35:55 +0000

    Update to platform spawning
    A lot of small tweaks, biggest change being updating the script to spawn platforms, could still use a bit of work

---
+++ Phoenix/Assets/Scripts/PlatformControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformControl : MonoBehaviour
+{
+    public GameObject platform;
+    public float yMin, yMax, xMin, xMax;
+
+    void Start()
+    {
+        
+    }
+
+    void platformSpawn()
+    {
+        float y = Random.Range(yMin, yMax);
+        float x = Random.Range(xMin, xMax);
+        Vector3 pos = new Vector3(x, y, 0);
+        Instantiate(platform, pos, transform.rotation);
+    }
+}
